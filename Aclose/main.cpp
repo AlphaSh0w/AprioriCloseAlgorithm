66e2ae6d1c736d218dca5cb4de946456f1864697
Alphashow <lastblast@outlook.fr>
2022-04-12 02:28:23 +0100

    Main variables are now more structured.

--- Aclose/main.cpp
+++ Aclose/main.cpp
@@ -4,31 +4,36 @@
 #include "Timer.h"
 #include "ACloseAlgorithm.h"
 
+std::string fileName = "ClassData.csv";
+std::vector<std::string> discretizeColumns={"raisedhands","VisITedResources","AnnouncementsView","Discussion"};
+std::vector<int> binSizes={ 10,10,10,10 };
+const float minSup = 0.2f;
+
 int main()
 {
     std::cout << "Loading csv ... ";
     Timer<std::chrono::milliseconds> timer{};
     //Load the airlines csv
-    DocumentLoader dataset{ "ClassData.csv" };
+    DocumentLoader dataset{ fileName };
     std::cout << "took " << timer.Mark() << " miliseconds\n";
 
     std::cout << "Discretizing ... ";
     timer.Mark();
-    dataset.Discretize<float>({"raisedhands","VisITedResources","AnnouncementsView","Discussion"}, {10,10,10,10});
+    dataset.Discretize<float>(discretizeColumns, binSizes);
     std::cout << "took " << timer.Mark() << " miliseconds\n";
 
     ACloseAlgorithm aClose{dataset.GetDocument()};
     std::cout << "Running AClose Algorithm ... ";
     timer.Mark();
-    aClose.Run(0.2f);
-    std::cout << "took " << timer.Mark() << " miliseconds\n";
-
-    std::cout << "\nDisplaying " << aClose.GetRules().size() << " rules : \n";
+    aClose.Run(minSup);
+    std::cout << "\n\nAlgorithm took " << timer.Mark() << " miliseconds.\n";
+    std::cout << "\nDisplaying " << aClose.GetRules().size() << " rules: \n";
     for (const auto& rule : aClose.GetRules())
     {
         auto columnNames = dataset.GetDocumentColumnNames();
         std::cout << rule.ToString(columnNames);
     }
+    std::cout << "Displayed " << aClose.GetRules().size() << " rules. \n";
     //Save the new document to disk
     //dataset.SaveDocument("ClassData-discretized.csv");
 }
